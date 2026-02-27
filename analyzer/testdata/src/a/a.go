43d6406a13f88a67bdd7c312904128b0eb2ab142
George <georgekushinov@mail.ru>
2026-02-27 15:05:29 +0300

    Скорректированы переносы строки в тесте a.go

--- analyzer/testdata/src/a/a.go
+++ analyzer/testdata/src/a/a.go
@@ -14,11 +14,11 @@ func test() {
 	slog.Info("user password: 123") // want "log message must not contain special characters or emoji" "log message contains potentially sensitive data"
 
 	logger := zap.NewExample()
-	logger.Info("Starting server")   // want "must start with lowercase"
-	logger.Info("запуск сервера")    // want "log message must be in English"
-	logger.Info("server started!!!") // want "log message must not contain special characters or emoji"
-
+	logger.Info("Starting server")    // want "must start with lowercase"
+	logger.Info("запуск сервера")     // want "log message must be in English"
+	logger.Info("server started!!!")  // want "log message must not contain special characters or emoji"
 	logger.Info("user password: 123") // want "log message must not contain special characters or emoji" "log message contains potentially sensitive data"
+
 	log.Println("Starting server")    // want "must start with lowercase"
 	log.Println("запуск сервера")     // want "log message must be in English"
 	log.Println("server started!!!")  // want "log message must not contain special characters or emoji"
